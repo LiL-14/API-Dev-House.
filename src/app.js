eae2568457e1537c3df87bd8f3a822248560b19e
LiL-14 <lucasballonje12@gmail.com>
2023-01-27 01:07:09 -0300

    feat :rocket: iniciando com o mongoDB

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import mongoose from 'mongoose';
+import routes from './routes';
+
+mongoose.set("strictQuery", true);
 
 class App{
     constructor(){
         this.server = express();
+
+        mongoose.connect("mongodb+srv://Lucas:lucas@devhouse.po27yov.mongodb.net/?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
         this.middlewares();
         this.routes();
@@ -18,4 +26,4 @@ class App{
     }
 }
 
-module.exports = new App().server 
+export default new App().server 
